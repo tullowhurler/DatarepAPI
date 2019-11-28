ab9075f3fa202386a8617c2b4bfa350c0d65f8d2
tullowhurler <G00364760@gmit.ie>
2019-11-28 21:14:07 +0000

    creating server

--- server1.py
+++ server1.py
@@ -2,10 +2,19 @@
 from flask import Flask 
 
 app = Flask(__name__, static_url_path='', static_folder='../')
+ #app = Flask(__name__)
  
-@app.route('/') 
-def index():     
-    return"Hello, World!" 
- 
+#@app.route('/') 
+#def index():     
+#    return"Hello, World!" 
+
+@app.route('cars')
+def getAll():
+    return "in getAll"
+
+@app.route('cars/<int:reg>')
+def getAll(id):
+    return "in find By REG for reg"+str(reg)
+
 if __name__ == '__main__' :     
     app.run(debug= True)
