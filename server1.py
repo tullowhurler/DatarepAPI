dde8d8ba65b76068d5362a08a4731f51151c246e
tullowhurler <G00364760@gmit.ie>
2019-12-13 19:09:46 +0000

    Adding more cars

--- server1.py
+++ server1.py
@@ -1,11 +1,14 @@
 #!flask/bin/python 
-from flask import Flask 
+from flask import Flask, jsonify
 
 app = Flask(__name__, static_url_path='', static_folder='../')
  
-cars=[{ "reg": 131, "make": "AudiA5", "Price":"15k","Mileage":"15000"
-
-}]
+cars=[ 
+    {"reg": 131, "make": "AudiA5", "Price":"15000","Mileage":"25000"},
+    {"reg": 132, "make": "AudiA6", "Price":"20000","Mileage":"20000"},
+    {"reg": 141, "make": "AudiA7", "Price":"30000","Mileage":"15000"}
+]
+NextId=4
 
  #app = Flask(__name__)
  
@@ -13,13 +16,18 @@ cars=[{ "reg": 131, "make": "AudiA5", "Price":"15k","Mileage":"15000"
 #def index():     
 #    return"Hello, World!" 
 
+#curl "http://127.0.0.1:5000/cars"
 @app.route('/cars')
 def getAll():
-    return "in getAll"
+    return jsonify(cars)
 
 @app.route('/cars/<int:reg>')
 def findByReg(reg):
-    return "in find By REG for reg"+str(reg)
+    foundCars = list(filter(lambda c: c['reg'] == reg, cars))
+    if len(foundCars) == 0:
+        return jsonify ({}) , 204 
+    
+    return jsonify (foundCars[0]) 
 
 @app.route('/cars', methods=['POST'])
 def create():
